a2e88e3236598a912ac32e0d32b152057245792b
sunilreddyg <sunilreddy.gajjala@gmail.com>
2022-01-26 11:56:37 +0530

    Variables class notes

---
+++ Project_198/src/mq/java/variables/Final_Variable.java
@@ -0,0 +1,21 @@
+package mq.java.variables;
+
+public class Final_Variable 
+{
+		int a=100;			//Allow to change variable value
+		final int b=200;    //Doesn't allow to change variable value
+
+		public static void main(String[] args) 
+		{
+			
+			Final_Variable obj=new Final_Variable();
+			System.out.println("Before change a value is --> "+obj.a);
+			obj.a=400;  //Overriding new value into variable
+			System.out.println("After change a value is --> "+obj.a);
+			
+			
+
+		}
+
+	}
+
---
+++ Project_198/src/mq/java/variables/VariableTypes.java
@@ -0,0 +1,20 @@
+package mq.java.variables;
+
+public class VariableTypes
+{
+	//Global Variable [Instant Variable]
+	int a=100;
+	String email="info.dsnr@gmail.com";
+	
+	//Global Variables [Class Variables/ Static Variables]
+	static int b=200;
+	static String name="mindq dnsr";
+	
+	
+	public void method1()
+	{
+		int c=300;   //Local Variable [Variable declared inside method block]
+		System.out.println(c);
+	}
+
+}
